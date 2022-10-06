6b224c69d15b48f4a6ae183f05c73760e8362cc4
Felipe Pereira <felipe.pereiralf95@gmail.com>
2022-10-06 15:15:19 -0300

    initial project

---
+++ TestUpd8.Mvc/TestUpd8.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestUpd8.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
